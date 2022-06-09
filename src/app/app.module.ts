25a4354e9b72cb032dc1a6248aac760b6b9e2afb
Sanjay Samantra <sanjaysamantra1@gmail.com>
2022-06-09 23:00:24 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CenterComponent } from './center/center.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { Demo1Component } from './demo1/demo1.component';
 
 @NgModule({
+  // components, pipes, directives
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CenterComponent,
+    NavbarComponent,
+    CarouselComponent,
+    Demo1Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  // modules
+  imports: [BrowserModule],
+  // export something to other module
+  exports: [],
+  // services
   providers: [],
-  bootstrap: [AppComponent]
+  // load - which component to load
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/demo1/demo1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo1',
+  template: `
+    <p>
+      demo1 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Demo1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
